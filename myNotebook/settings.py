6a5433f00063f41bd9823d62ce3eee2bfe228ce8
jacychutw <jacychu.tw@gmail.com>
2024-09-23 11:46:11 +0800

    reverse db

--- myNotebook/settings.py
+++ myNotebook/settings.py
@@ -80,25 +80,12 @@ WSGI_APPLICATION = 'myNotebook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_mynotebook_db',
-        'USER': 'django_mynotebook_db_user',
-        'PASSWORD': '7UK5kk5VqXwwIjsSs9KvjyENQHeKkDzz',
-        'HOST': 'dpg-crodlk56l47c73fmtvs0-a',
-        'PORT': '5432',  # PostgreSQL 預設端口
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
